214c5d96bc7f26ba8f9e217bc3750a3e1b364a1e
anton <none@none>
2012-11-28 14:35:46 +0100

    One more require_relative

--- lib/pdf_devastator/xfa.rb
+++ lib/pdf_devastator/xfa.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 require 'java'
 require 'pdf_devastator/form'
 
-require_relative '../../vendor/iText-4.2.0'
+require File.join(File.dirname(__FILE__), '../../vendor/iText-4.2.0')
 
 java_import "com.lowagie.text.pdf.AcroFields"
 java_import "com.lowagie.text.pdf.PdfArray"
